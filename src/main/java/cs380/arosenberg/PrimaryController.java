a0ed216183a34881805a24e9496d01492eaecad4
bird <birdluker@gmail.com>
2023-03-06 12:20:27 -0600

    added point totals, did NOT do tests sorry

--- src/main/java/cs380/arosenberg/PrimaryController.java
+++ src/main/java/cs380/arosenberg/PrimaryController.java
@@ -5,8 +5,33 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+	@FXML
+	private void switchToSecondary() throws IOException {
+		App.setRoot("secondary");
+	}
+
+	public String word;
+
+	public int assignPointValue(String word) {
+    	//the pangram is worth an additional seven points. 
+    	//if the unique letter count is 7, then an additional 7 points should be awarded
+    	String orgInput = word.toLowerCase();
+        int count = 0;
+        int stringLength = word.length();
+        for( int i = 0; i<stringLength; i++){
+            for(int j = 2; j > j-i-1; j--){
+                char temp = orgInput.charAt(i);
+                if (temp == orgInput.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        int bonus = 0;
+    if (stringLength == 7) {
+    	 bonus = 7;
     }
+    	//the nyt says that all words are worth the same amount of points as their length
+    	int length = word.length();
+    	return length + bonus;
+	}
 }
