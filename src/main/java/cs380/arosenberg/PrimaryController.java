ace6a17dd6552d3f83f0ff5a544a0221a562cfb0
Alan Rosenberg <arosenberg20@austincollege.edu>
2023-03-08 21:02:54 -0600

    made method stubs in primary controller

--- src/main/java/cs380/arosenberg/PrimaryController.java
+++ src/main/java/cs380/arosenberg/PrimaryController.java
@@ -2,8 +2,72 @@ package cs380.arosenberg;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
+    @FXML
+    TextField textInput;
     
+    @FXML
+    Button ltrBtn1;
+    
+    @FXML
+    Button ltrBtn2;
+    
+    @FXML
+    Button ltrBtn3;
+    
+    @FXML
+    Button ltrBtn4;
+    
+    @FXML
+    Button ltrBtn5;
+    
+    @FXML
+    Button ltrBtn6;
+    
+    @FXML
+    Button ltrBtn7;
+    
+    @FXML
+    Button deleteBtn;
+    
+    @FXML
+    Button shuffleBtn;
+    
+    @FXML
+    Button enterBtn;
+    
+    @FXML
+    Label scoreLbl;
+    
+    @FXML
+    Label countLbl;
+    
+    @FXML
+    ListView<String> wordList;
+    
+    @FXML
+    public void ltrBtnDo() {
+    	// TODO give letter buttons functionality
+    }
+    
+    @FXML
+    public void deleteBtnDo() {
+    	// TODO give delete button functionality
+    }
+    
+    @FXML
+    public void shuffleBtnDo() {
+    	// TODO give shuffle button functionality
+    }
+    
+    @FXML
+    public void enterBtnDo() {
+    	// TODO give enter button functionality
+    }
 }
